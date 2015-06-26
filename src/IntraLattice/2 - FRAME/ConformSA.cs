c4421028aaa0be4746dae36e2e608aa2bb7c0f98
Aidan <dnkrtz@aidankurtz.com>
2015-06-26 00:09:05 -0400

    both conforms now work for custom cells

--- src/IntraLattice/2 - FRAME/ConformSA.cs
+++ src/IntraLattice/2 - FRAME/ConformSA.cs
@@ -4,23 +4,26 @@ using Grasshopper.Kernel;
 using Rhino.Geometry;
 using Grasshopper.Kernel.Data;
 using Grasshopper.Kernel.Types;
-using Grasshopper.Kernel.Special;
+using Rhino.Geometry.Intersect;
 using Rhino.Collections;
 using Rhino;
 
-// This component generates a conformal lattice grid between two surfaces.
-// =======================================================================
-// Assumption : The surfaces are oriented in the same direction (for UV-Map indices)
+// This component generates a conformal lattice grid between a surface and an axis
+// ===============================================================================
+// The axis can be an open curve or a closed curve. Of course, it may also be a straight line.
+// The surface does not need to loop a full 360 degrees around the axis.
+// Our implementation assumes that the axis is a set of U parameters, thus it should be aligned with U parameters of the surface.
+// The flipUV input allows the user to swap U and V parameters of the surface.
 
 // Written by Aidan Kurtz (http://aidankurtz.com)
 
 namespace IntraLattice
 {
-    public class GridConformSS : GH_Component
+    public class ConformSA : GH_Component
     {
-        public GridConformSS()
-            : base("Conform Surface-Surface", "ConformSS",
-                "Generates a conforming point grid between two surfaces.",
+        public ConformSA()
+            : base("Conform Surface-Axis", "ConformSA",
+                "Generates conforming lattice grid between a surface and an axis",
                 "IntraLattice2", "Grid")
         {
         }
@@ -28,9 +31,9 @@ namespace IntraLattice
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddCurveParameter("Topology", "Topo", "Unit cell topology", GH_ParamAccess.list);
-            pManager.AddSurfaceParameter("Surface 1", "S1", "First bounding surface", GH_ParamAccess.item);
-            pManager.AddSurfaceParameter("Surface 2", "S2", "Second bounding surface", GH_ParamAccess.item);
-            pManager.AddBooleanParameter("Flip UV", "FlipUV", "Flip the UV parameters (for alignment purposes)", GH_ParamAccess.item, false); // default value is false
+            pManager.AddSurfaceParameter("Surface", "Surface", "Surface to conform to", GH_ParamAccess.item);
+            pManager.AddCurveParameter("Axis", "A", "Axis (may be curved)", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Flip UV", "FlipUV", "Flip the U and V parameters on the surface", GH_ParamAccess.item, false); // default value is true
             pManager.AddNumberParameter("Number u", "Nu", "Number of unit cells (u)", GH_ParamAccess.item, 5);
             pManager.AddNumberParameter("Number v", "Nv", "Number of unit cells (v)", GH_ParamAccess.item, 5);
             pManager.AddNumberParameter("Number w", "Nw", "Number of unit cells (w)", GH_ParamAccess.item, 5);
@@ -40,16 +43,16 @@ namespace IntraLattice
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddPointParameter("Nodes", "Nodes", "Lattice Nodes", GH_ParamAccess.tree);
-            pManager.AddCurveParameter("Struts", "Struts", "Strut curve network", GH_ParamAccess.list);
+            pManager.AddPointParameter("Nodes", "Nodes", "Nodes", GH_ParamAccess.tree);
+            pManager.AddCurveParameter("Struts", "Struts", "Struts", GH_ParamAccess.list);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             // 1. Declare placeholder variables
             List<Curve> topology = new List<Curve>();
-            Surface s1 = null;
-            Surface s2 = null;
+            Surface surface = null;
+            Curve axis = null;
             bool flipUV = false;
             double nU = 0;
             double nV = 0;
@@ -57,10 +60,10 @@ namespace IntraLattice
             bool morphed = false;
             double morphFactor = 0;
 
-            // 2. Attempt to fetch data
+            // 2.   Attempt to fetch data
             if (!DA.GetDataList(0, topology)) { return; }
-            if (!DA.GetData(1, ref s1)) { return; }
-            if (!DA.GetData(2, ref s2)) { return; }
+            if (!DA.GetData(1, ref surface)) { return; }
+            if (!DA.GetData(2, ref axis)) { return; }
             if (!DA.GetData(3, ref flipUV)) { return; }
             if (!DA.GetData(4, ref nU)) { return; }
             if (!DA.GetData(5, ref nV)) { return; }
@@ -68,30 +71,29 @@ namespace IntraLattice
             if (!DA.GetData(7, ref morphed)) { return; }
             if (!DA.GetData(8, ref morphFactor)) { return; }
 
-            // 3. Validate data
+            // 3. Validate data, if invalid, abort
             if (topology.Count < 2) { return; }
-            if (!s1.IsValid) { return; }
-            if (!s2.IsValid) { return; }
+            if (!surface.IsValid) { return; }
+            if (!axis.IsValid) { return; }
             if (nU == 0) { return; }
             if (nV == 0) { return; }
             if (nW == 0) { return; }
 
             // 4. Initialize the grid tree and derivatives tree
-            var nodeTree = new GH_Structure<GH_Point>();     // will contain lattice nodes
-            var derivTree = new GH_Structure<GH_Vector>();   // will contain derivatives (du,dv) in a parallel tree
+            var nodeTree = new GH_Structure<GH_Point>();
+            var derivTree = new GH_Structure<GH_Vector>();
 
-            // 5. Flip the UV parameters a surface if specified
-            if (flipUV) s1 = s1.Transpose();
-            
+            // 5. Flip the UV parameters if specified
+            if (flipUV) surface = surface.Transpose();
+
             // 6. Package the number of cells in each direction into an array
             double[] N = new double[3] { nU, nV, nW };
 
             // 7. Normalize the UV-domain
-            Interval normalizedDomain = new Interval(0,1);
-            s1.SetDomain(0, normalizedDomain); // s1 u-direction
-            s1.SetDomain(1, normalizedDomain); // s1 v-direction
-            s2.SetDomain(0, normalizedDomain); // s2 u-direction
-            s2.SetDomain(1, normalizedDomain); // s2 v-direction
+            Interval normalizedDomain = new Interval(0, 1);
+            surface.SetDomain(0, normalizedDomain); // surface u-direction
+            surface.SetDomain(1, normalizedDomain); // surface v-direction
+            axis.Domain = normalizedDomain; // axis (u-direction)
 
             // 8. Prepare normalized unit cell topology
             var cellNodes = new Point3dList();
@@ -99,51 +101,60 @@ namespace IntraLattice
             TopologyTools.Topologize(ref topology, ref cellNodes, ref cellStruts);  // converts list of lines into an adjacency list format (cellNodes and cellStruts)
             TopologyTools.NormaliseTopology(ref cellNodes); // normalizes the unit cell (scaled to unit size and moved to origin)
 
-            // 9. Map nodes to design space
-            //    Loop through the uvw cell grid
+            // 9. Divide axis into equal segments, get curve parameters
+            double[] curveParams = axis.DivideByCount((int)nU, true);
+            double uStep = curveParams[1] - curveParams[0];
+            //    If axis is closed curve, add last parameter to close the loop
+            if (axis.IsClosed) curveParams[curveParams.Length] = curveParams[0]; 
+
+            // 10. Let's create the actual point grid now
+            //
             for (int u = 0; u <= N[0]; u++)
             {
                 for (int v = 0; v <= N[1]; v++)
                 {
-                    // this loop maps each node in the cell onto the UV-surface maps
+                    // this loop maps each node in the cell onto the UV-surface map and axis (U)
                     for (int nodeIndex = 0; nodeIndex < cellNodes.Count; nodeIndex++)
                     {
-                        Point3d pt1; Vector3d[] derivatives1; // initialize for surface 1
-                        Point3d pt2; Vector3d[] derivatives2; // initialize for surface 2
-
+                        // local node position within cell
                         double usub = cellNodes[nodeIndex].X; // u-position within unit cell
                         double vsub = cellNodes[nodeIndex].Y; // v-position within unit cell
                         double wsub = cellNodes[nodeIndex].Z; // w-position within unit cell
 
-                        // evaluate point and its derivatives on both surfaces
-                        s1.Evaluate((u+usub) / N[0], (v+vsub) / N[1], 2, out pt1, out derivatives1);
-                        s2.Evaluate((u+usub) / N[0], (v+vsub) / N[1], 2, out pt2, out derivatives2);
+                        // evaluate the point on the axis
+                        Point3d pt1;
+                        Point3d pt2; Vector3d[] derivatives; // initialize for surface 2
+
+                        // evaluate point and its derivatives on the axis and the surface
+                        pt1 = axis.PointAt(curveParams[u] + usub / N[0]);
+                        surface.Evaluate((u + usub) / N[0], (v + vsub) / N[1], 2, out pt2, out derivatives);
+                        derivatives[2] = surface.NormalAt((u + usub) / N[0], (v + vsub) / N[1]);
 
                         // create vector joining the two points (this is our w-range)
                         Vector3d wVect = pt2 - pt1;
 
-                        // create grid points on and between surfaces
+                        // create grid points on and between surface-axis
                         for (int w = 0; w <= N[2]; w++)
                         {
                             GH_Path treePath = new GH_Path(u, v, w, nodeIndex);    // u,v,w is the cell grid. the last index is for different nodes in each cell.
 
                             // these conditionals enforce the boundary, no nodes are created beyond the upper boundary
-                            if ( u==N[0] && usub!=0) continue;
-                            if ( v==N[1] && vsub!=0) continue;
-                            if ( w==N[2] && wsub!=0) continue;
+                            if (u == N[0] && usub != 0) continue;
+                            if (v == N[1] && vsub != 0) continue;
+                            if (w == N[2] && wsub != 0) continue;
 
                             // create the node, accounting for the position along the w-direction
                             Point3d newPt = pt1 + wVect * (w + wsub) / N[2];
                             nodeTree.Append(new GH_Point(newPt), treePath);
 
                             // for each of the 2 directional directives (du and dv)
-                            for (int derivIndex = 0; derivIndex < 2; derivIndex++)
+                            for (int derivIndex = 0; derivIndex < 3; derivIndex++)
                             {
-                                // compute the interpolated derivative (need interpolation for in-between surfaces)
-                                double interpolationFactor = (w + wsub) / N[2];
-                                Vector3d deriv = derivatives1[derivIndex] + interpolationFactor * (derivatives2[derivIndex] - derivatives1[derivIndex]);
+                                // compute the uv-derivatives
+                                // decrease the amplitude of the derivative vector as we approach the axis
+                                Vector3d deriv = derivatives[derivIndex] * (w + wsub) / N[2];
                                 // this division scales the derivatives (gives better control of the bezier curves)
-                                deriv = deriv / (morphFactor * N[derivIndex]);
+                                if (derivIndex<2) deriv = deriv / (morphFactor * N[derivIndex]);
                                 derivTree.Append(new GH_Vector(deriv), treePath);
                             }
                         }
@@ -175,19 +186,18 @@ namespace IntraLattice
                                 Point3d node1 = nodeTree[startPath][0].Value;
                                 Point3d node2 = nodeTree[endPath][0].Value;
 
-                                // get direction vector from the normalized 'cellNodes'
-                                Vector3d directionVector1 = new Vector3d(cellNodes[cellStrut.J] - cellNodes[cellStrut.I]);
-                                directionVector1.Unitize();
-
                                 // if user requested morphing, we need to compute bezier curve struts
                                 if (morphed)
                                 {
                                     // compute directional derivatives
+                                    // get direction vector from the normalized 'cellNodes'
+                                    Vector3d directionVector1 = new Vector3d(cellNodes[cellStrut.J] - cellNodes[cellStrut.I]);
+                                    directionVector1.Unitize();
                                     // we use the du and dv derivatives as the basis for the directional derivative
-                                    Vector3d deriv1 = derivTree[startPath][0].Value * directionVector1.X + derivTree[startPath][1].Value * directionVector1.Y;
+                                    Vector3d deriv1 = derivTree[startPath][0].Value * directionVector1.X + derivTree[startPath][1].Value * directionVector1.Y + derivTree[startPath][2].Value * directionVector1.Z;
                                     // same process for node2, but reverse the direction vector
                                     Vector3d directionVector2 = - directionVector1;
-                                    Vector3d deriv2 = derivTree[endPath][0].Value * directionVector2.X + derivTree[endPath][1].Value * directionVector2.Y;
+                                    Vector3d deriv2 = derivTree[endPath][0].Value * directionVector2.X + derivTree[endPath][1].Value * directionVector2.Y + derivTree[endPath][2].Value * directionVector2.Z;
 
                                     // now we have everything we need to build a bezier curve
                                     List<Point3d> controlPoints = new List<Point3d>();
@@ -212,7 +222,7 @@ namespace IntraLattice
                 }
             }
 
-            // 11. Set output
+            // 8. Set output
             DA.SetDataTree(0, nodeTree);
             DA.SetDataList(1, struts);
 
@@ -244,7 +254,7 @@ namespace IntraLattice
         /// </summary>
         public override Guid ComponentGuid
         {
-            get { return new Guid("{ac0814b4-00e7-4efb-add5-e845a831c6da}"); }
+            get { return new Guid("{e0e8a858-66bd-4145-b173-23dc2e247206}"); }
         }
     }
 }
